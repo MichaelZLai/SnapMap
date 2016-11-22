7a122c2a8ade4142ad0d5543bd5765474abd5d3d
Michael Lai <mike24567@gmail.com>
2016-11-22 10:18:49 -0500

    able to seed database

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 var mongoose    = require("mongoose");
 //connect to MongoDB
-mongoose.connect("mongod://localhost/snapmap")
+mongoose.connect("mongodb://localhost/snapmap")
 //allows mongoose to have promises
 mongoose.Promise = global.Promise
 
 //exports mongoose
-module.exports = mongoose 
+module.exports = mongoose
