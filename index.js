b525255c67d7d3972705dde6fc29194e1fd41600
Michael Lai <mike24567@gmail.com>
2016-11-22 13:44:45 -0500

    made angular items

---
+++ index.js
@@ -0,0 +1,24 @@
+var express       = require("express");
+var app           = express();
+var mongoose      = require("./db/connection");
+var models        = require("./db/models");
+var bodyParser    = require("body-parser");
+var Trip          = mongoose.model("Trip");
+var Marker        = mongoose.model("Marker");
+
+
+//utilize files in the public folder
+app.use(express.static("public"));
+
+//set view engine to be hbs
+app.set("view engine", "hbs")
+
+//set up port and listen to local host
+app.set("port", process.env.PORT || 5000)
+app.listen(app.get("port"), _ => {
+  console.log("Consequences....")
+})
+
+//use body-parser for handlebars and generating api responses
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({extended:true}))
