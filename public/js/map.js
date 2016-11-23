0cfe07f81acd301392f74ee940eb298dd3ab251a
Michael Lai <mike24567@gmail.com>
2016-11-23 08:57:57 -0500

    cleaned up map js

--- public/js/map.js
+++ public/js/map.js
@@ -7,45 +7,11 @@
       mapTypeId: "terrain"
     });
 
-    // var mexico1 = {lat: 19.434331, lng: -99.140164};
-    // var mexico2 = {lat: 19.432602, lng: -99.133205};
-    //
-    //
-    // var mexico1img = {
-    //   url: "https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/15068436_10154204506938790_8493317963348433027_o.jpg",
-    //   scaledSize: new google.maps.Size(40, 40),
-    // };
-    // var mexico2img = {
-    //   url: "https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14102928_10153961036873790_6301174510094312346_o.jpg",
-    //   scaledSize: new google.maps.Size(40, 40),
-    // };
-    //
-    //
-    // var marker1 = new google.maps.Marker({
-    //   position: mexico1,
-    //   map: map,
-    //   icon: mexico1img
-    // });
-    // var marker2 = new google.maps.Marker({
-    //   position: mexico2,
-    //   map: map,
-    //   icon: mexico2img
-    // });
-    // var contentString =
-    //   '<div id="content">'+
-    //   '<img src="https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14102928_10153961036873790_6301174510094312346_o.jpg" style="width:400px;height:auto;">'+
-    //   '</div>';
-    //
-    // var infowindow = new google.maps.InfoWindow({
-    //   content: contentString
-    // });
-    // marker2.addListener('click', function() {
-    // infowindow.open(map, marker2);
-    // });
     //sets the markers on the map
     setMarkers(map);
   }
 
+  //hardcoded mexico data
   var mexico = [
     {title: "atrio", lat: 19.434331, lng: -99.140164, url: "https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/15068436_10154204506938790_8493317963348433027_o.jpg"},
     {title: "zocalo", lat: 19.432602, lng: -99.133205, url: "https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14102928_10153961036873790_6301174510094312346_o.jpg"},
