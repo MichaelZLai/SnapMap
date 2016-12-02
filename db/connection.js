029385b55f23a0415e76f7cf10bc5ec281c6a31d
Michael Lai <mike24567@gmail.com>
2016-12-02 11:03:04 -0500

    revised local connection

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 var mongoose    = require("mongoose");
 //connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/snapmap")
 //allows mongoose to have promises
 mongoose.Promise = global.Promise
 
