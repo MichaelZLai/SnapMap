e62832ff6b66ca6e6c7437af01023d3fcfe64cdf
Michael Lai <mike24567@gmail.com>
2016-12-02 09:46:53 -0500

    added mongolab

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 var mongoose    = require("mongoose");
 //connect to MongoDB
-mongoose.connect("mongodb://localhost/snapmap")
+mongoose.connect(process.env.MONGODB_URI)
 //allows mongoose to have promises
 mongoose.Promise = global.Promise
 
